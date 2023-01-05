fae0c8b8c8cad0607a425164d77a134364af9cc5
Mohamad <mohamad93ja@gmail.com>
2023-01-06 00:00:27 +0100

    Updated script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env sh
-# abort on errors
+
 set -e
-# build
-yarn run build
-# navigate into the build output directory
+
+npm run build
+
 cd dist
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
